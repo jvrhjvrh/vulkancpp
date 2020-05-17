e7114ab57e4a91e0298d3cf6e41e5ec1ab1678c9
jvrh <joao.hilario@monitoratec.com.br>
2020-05-17 15:46:55 -0300

    commit inicial

---
+++ build/CMakeFiles/MyVulkanTutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyVulkanTutorial.dir/main.cpp.o"
+  "MyVulkanTutorial"
+  "MyVulkanTutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyVulkanTutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
